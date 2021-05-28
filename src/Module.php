2942c1708c75282b149947f39d3696e64ad697d0
胡争辉 <hu@daonao.com>
2021-05-28 15:05:17 +0800

    add Module

---
+++ src/Module.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace huzhenghui\hello;
+
+use Yii;
+use yii\base\BootstrapInterface;
+
+class Module extends \yii\base\Module implements BootstrapInterface
+{
+    public function bootstrap($app)
+    {
+        return null;
+    }
+}
